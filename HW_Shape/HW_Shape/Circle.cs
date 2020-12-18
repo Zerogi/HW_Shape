9805f49c913ffb1cbaf8b009388fd0207fcc4b64
Zerogi <57771793+Zerogi@users.noreply.github.com>
2020-12-18 11:34:59 +0300

    Add files via upload

---
+++ HW_Shape/HW_Shape/Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace HW_Shape
+{
+    class Circle : Shape
+    {
+        public string Name { get; set; }
+        public double Radius { get; set; }
+        public new double Perimetr { get; set; }
+        public new double Square { get; set; }
+
+        public Circle(double radius)
+        {
+            Name = "Circle";
+            Radius = radius;
+            Perimetr = 2 * Math.PI * Radius;
+            Square = Math.PI * (Radius * Radius);
+        }
+    }
+}
